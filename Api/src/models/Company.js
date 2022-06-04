ec4b261887540b0555e44d9c14f63b4e302de51a
BUFFON <55423389+Sebastian-RP@users.noreply.github.com>
2022-06-03 21:07:44 -0500

    carpeta modelos nueva-nuevos modelos de usuarios y companias

---
+++ Api/src/models/Company.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                isEmail: true,
+            }
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        propietary_name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        url:{
+            type: DataTypes.STRING,
+            validate:{
+                isUrl: true
+            }
+        },
+        image:{
+            type: DataTypes.STRING,
+            validate:{
+                isUrl: true,
+            }
+        },
+        nationality:{
+            type: DataTypes.STRING
+        },
+        description:{
+            type: DataTypes.TEXT
+        },
+        employees:{
+            type: DataTypes.ARRAY(DataTypes.INTEGER) //id de los empleados asociados a dicha empresa
+        },
+        posts:{
+            type: DataTypes.ARRAY(DataTypes.INTEGER) //id de los post vinculados a la empresa
+        }
+    })
+}
