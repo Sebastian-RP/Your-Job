95f7fe6fb23367e69635f80268b90bc03b68d36d
Sebastián Zanatta <zanattasebas@gmail.com>
2022-06-06 19:05:47 -0300

    Creada la ruta para tener las tecnologias de la DB y el mock

--- Api/src/Controllers/TechnologyController.js
+++ Api/src/Controllers/TechnologyController.js
@@ -1,7 +1,15 @@
 const { Technology } = require("../db.js")
 
+let tecnologias = ["Javascript", "React", "Redux", "HTML5", "CSS3", "Boostrap", "Jquery"]
+
+
 const getTechnologies = async () => {
     try{
+        let technologiesToDB = tecnologias.forEach(tech=> {
+            Technology.create({
+                where: {name : tech.trim()}
+            })
+        })
         let technologies = Technology.findAll()
         technologies.map(x => x.name).sort()
         return technologies;
