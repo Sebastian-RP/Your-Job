92949e0e88f171a666e0f1ac969b8687670bd5fa
Tomshaster <tomascartagena00@gmail.com>
2022-06-06 16:51:31 -0300

    Agregado el module exports
    Agregué los module.export en companyRoute y userRoute

--- Api/src/Routes/userRoute.js
+++ Api/src/Routes/userRoute.js
@@ -1,31 +1,34 @@
 const { Router } = require("express");
-const { createUser } = require("../Controllers/UserController.js")
+const { createUser } = require("../Controllers/UserController.js");
 
-const router = Router()
+const router = Router();
 
-router.post('/login', async (req,res) => {
-    const { 
-        email,
-        name,
-        employment_status,
-        age,
-        image,
-        description,
-        technologies,
-        nationality,
-        url,
-        cv } = req.body
-    const newUser = await createUser(
-        email,
-        name,
-        employment_status,
-        age,
-        image,
-        description,
-        technologies,
-        nationality,
-        url,
-        cv
-    )
-    return res.status(200).send(newUser)
+router.post("/login", async (req, res) => {
+  const {
+    email,
+    name,
+    employment_status,
+    age,
+    image,
+    description,
+    technologies,
+    nationality,
+    url,
+    cv,
+  } = req.body;
+  const newUser = await createUser(
+    email,
+    name,
+    employment_status,
+    age,
+    image,
+    description,
+    technologies,
+    nationality,
+    url,
+    cv
+  );
+  return res.status(200).send(newUser);
 });
+
+module.exports = router;
