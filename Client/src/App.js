99d625c9cb090c33fa7dbe0978e1d5ac060caa24
grexon-hack <jsarabialugo@gmail.com>
2022-06-07 17:56:26 -0500

    fix(changes of route)

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SearchBar from "./Components/SearchBar/SearchBar";
 import Users from "./Pages/Users/Users";
 import Login from "./Pages/Login/Login";
-import PerfilUser from "./Pages/perfilUser/perfilUser";
+
 
 function App() {
   return (
@@ -18,7 +18,6 @@ function App() {
       />
       <Route path={"/login"} element={<Login />} />
       <Route path={"/users/:user"} element={<Users />} />
-      <Route path={"/perfil/user"} element={<PerfilUser/>}/>
     </Routes>
   );
 }
