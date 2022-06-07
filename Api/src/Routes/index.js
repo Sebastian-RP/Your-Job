2b8b81f89b607d22b5629f6d3a61c0fb8e961520
Luciano plaza <95538929+Luciano-plaza@users.noreply.github.com>
2022-06-07 18:47:59 -0300

    feat(api/routePost): Add route whit controller for create the job

--- Api/src/Routes/index.js
+++ Api/src/Routes/index.js
@@ -3,12 +3,13 @@ const { Router } = require('express');;
 const userRoutes = require('./userRoute.js');
 const companyRoutes = require('./companyRoute.js');
 const technologyRoutes = require('./technologyRoute.js');
-
+const CompanyPost = require('./CompanyPost.js')
 
 const router = Router()
 
 router.use("/users", userRoutes);
 router.use("/company", companyRoutes);
 router.use("/technology", technologyRoutes);
+router.use("/company", CompanyPost);
 
 module.exports = router;
