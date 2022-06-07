d4bc0efe9681006e11aea78a1a9bef468de1b2d8
BUFFON <55423389+Sebastian-RP@users.noreply.github.com>
2022-06-07 15:37:21 -0500

    conectar las rutas con app.js, creacion de controlador ruta para companyPost

--- Api/src/Routes/index.js
+++ Api/src/Routes/index.js
@@ -1,14 +1,19 @@
-const { Router } = require('express');;
+const { Router } = require('express');
 
 const userRoutes = require('./userRoute.js');
 const companyRoutes = require('./companyRoute.js');
 const technologyRoutes = require('./technologyRoute.js');
-
+const companyPostRoutes = require('./companyPostRoute.js')
 
 const router = Router()
 
 router.use("/users", userRoutes);
 router.use("/company", companyRoutes);
 router.use("/technology", technologyRoutes);
+router.use("/companyPost", companyPostRoutes);
+
+router.use("*", (req, res)=>{
+    res.status(404).send({error: "page not found"});
+});
 
 module.exports = router;
