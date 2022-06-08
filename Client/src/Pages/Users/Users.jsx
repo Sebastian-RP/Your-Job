802ee6c680016f580a09877e0584496cd4eb7beb
grexon-hack <jsarabialugo@gmail.com>
2022-06-07 20:33:14 -0500

    feat(/users/:user):informacion mokeada

--- Client/src/Pages/Users/Users.jsx
+++ Client/src/Pages/Users/Users.jsx
@@ -1,49 +1,57 @@
 import React, { useEffect } from "react";
-import image from "./perfilPicture.png";
 import style from "./perfil.module.css";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { getUserInfo } from "../../Redux/Actions/Actions";
 
 export default function Users() {
-
   // esto es para poder mokear la info ya que esta action se deberia de hacer
   // al hacer el login ya deberia de pasar la informacion al reducer.
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUserInfo());
-  },[])
+  }, []);
   //----------------------------------
+  const selector = useSelector((state) => state.user);
   const { user } = useParams();
   return (
-    
-      
-      <div className={style.containerPerfil}>
+    <div className={style.containerPerfil}>
       <div className={style.header}>
         <div className={style.picture}>
-          <img src={image} alt="perfil-picture" />
+          <img src={selector.image} alt="perfil-picture" />
           <h2>{user}</h2>
         </div>
         <div>
-        <Link to={"/"}>Go to home</Link>
+          <Link to={"/"}>Go to home</Link>
         </div>
       </div>
       <div className={style.suggestions}>
-          <h2>Suggestions</h2>
+        <h2>Suggestions</h2>
       </div>
       <div className={style.perfilInfo}>
-          <div className={style.about}>
-            <h2>About</h2>
-
+        <div className={style.about}>
+          <h2>About</h2>
+          <p>Email: {selector.email}</p>
+          <p>Status: {selector.employment_status}</p>
+          <p>Age: {selector.age}</p>
+          <p>Nationality: {selector.nationality}</p>
+        </div>
+        <div className={style.info}>
+          <h2>info</h2>
+          <p>{selector.description}</p>
+          <hr />
+          <div>
+            <ul>
+              {selector.technologies.map((data, index) => {
+                return <li key={index}>{data}</li>;
+              })}
+            </ul>
           </div>
-          <div className={style.info}>
-            <h2>info</h2>
-
+          <div style={{textAlign:'right'}}>
+            <a href={selector.CVurl}>download CV</a>
           </div>
+        </div>
       </div>
     </div>
-
-      
-  
   );
 }
