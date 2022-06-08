293d1f2fec37bf10b819c3f3122ab687447f6ef4
José Samuel Peña <71469963+Jspena01@users.noreply.github.com>
2022-06-08 12:15:09 -0300

    Update Actions.js

--- Client/src/Redux/Actions/Actions.js
+++ Client/src/Redux/Actions/Actions.js
@@ -1,13 +1,11 @@
-import {db} from "../../Components/Firebase/credenciales.js";
-import {collection, getDocs} from "firebase/firestore";
-
 export const GET_ALL_EMPLOYEES = "GET_ALL_EMPLOYEES";
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
-
 export function getAllEmployees() {
   return { type: GET_ALL_EMPLOYEES, payload: ["empleado1", "empleado2"] };
 }
 
+import {db} from "../../Components/Firebase/credenciales.js";
+import {collection, getDocs} from "firebase/firestore";
 
 export async function getAllProducts() {
   const collectionRef = collection(db, "products");
