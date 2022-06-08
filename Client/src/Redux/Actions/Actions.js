3f2f5fa733fd78a9d11ea998f639883e8ecb0c81
juanmabascu997 <jmb972012@gmail.com>
2022-06-08 15:15:53 -0300

    Update Actions.js

--- Client/src/Redux/Actions/Actions.js
+++ Client/src/Redux/Actions/Actions.js
@@ -1,12 +1,11 @@
 import { db } from "../../Components/Firebase/credenciales.js";
-import { collection, getDocs } from "firebase/firestore";
 import axios from "axios";
-import {db} from "../../Components/Firebase/credenciales.js";
 import {collection, getDocs, query, where} from "firebase/firestore";
 
 export const GET_ALL_EMPLOYEES = "GET_ALL_EMPLOYEES";
 export const GET_USER_INFO = "GET_USER_INFO";
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+
 export function getAllEmployees() {
   return { type: GET_ALL_EMPLOYEES, payload: ["empleado1", "empleado2"] };
 }
