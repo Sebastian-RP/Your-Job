2d3efa5b13202671541a851364928779acb1dd0b
Tomshaster <tomascartagena00@gmail.com>
2022-06-09 17:31:13 -0300

    Creada ruta de GetAllCompanies

--- Api/src/Controllers/CompanyController.js
+++ Api/src/Controllers/CompanyController.js
@@ -1,36 +1,42 @@
-const { Company } = require("../db.js")
+const { Company } = require("../db.js");
 
-const createCompany = async ( 
-    email,
-    name,
-    phone,
-    propietary_name,
-    address,
-    url,
-    image,
-    nationality,
-    description,
-    employees) => {
-        try{
-            const newCompany = await Company.create({
-                email,
-                name,
-                phone,
-                propietary_name,
-                address,
-                url,
-                image,
-                nationality,
-                description,
-                employees
-            });
-            return "Company account created"
-        }   catch (error) {
-            console.error("Error in createCompany:", error.message);
-        }
-    };
+const createCompany = async (
+  email,
+  name,
+  phone,
+  propietary_name,
+  address,
+  url,
+  image,
+  nationality,
+  description,
+  employees
+) => {
+  try {
+    const newCompany = await Company.create({
+      email,
+      name,
+      phone,
+      propietary_name,
+      address,
+      url,
+      image,
+      nationality,
+      description,
+      employees,
+    });
+    return "Company account created";
+  } catch (error) {
+    console.error("Error in createCompany:", error.message);
+  }
+};
 
+const getCompanies = async () => {
+  const companies = await Company.findAll();
+  return companies;
+};
 
 module.exports = {
-    createCompany
-}
+  createCompany,
+  getCompanies,
+};
--- Api/src/Routes/companyRoute.js
+++ Api/src/Routes/companyRoute.js
@@ -1,11 +1,40 @@
 const { Router } = require("express");
-const { createCompany } = require("../Controllers/CompanyController.js");
+const {
+  createCompany,
+  getCompanies,
+} = require("../Controllers/CompanyController.js");
 
 const router = Router();
 
+router.get("/", async (req, res) => {
+  res.send(await getCompanies());
+});
+
 router.post("/login", async (req, res) => {
-  const { email, name, phone, propietary_name, address, url, image, nationality, description, employees } = req.body;
-  const newCompany = await createCompany(email, name, phone, propietary_name, address, url, image, nationality, description, employees);
+  const {
+    email,
+    name,
+    phone,
+    propietary_name,
+    address,
+    url,
+    image,
+    nationality,
+    description,
+    employees,
+  } = req.body;
+  const newCompany = await createCompany(
+    email,
+    name,
+    phone,
+    propietary_name,
+    address,
+    url,
+    image,
+    nationality,
+    description,
+    employees
+  );
   return res.status(200).send(newCompany);
 });
 
