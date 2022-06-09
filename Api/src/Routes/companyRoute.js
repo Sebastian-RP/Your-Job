2cbdbfc4e68c9badbacc4702f37d2a4f9001d110
Tomshaster <tomascartagena00@gmail.com>
2022-06-09 18:09:58 -0300

    Creada Ruta para conseguir una empresa y para conseguir todos los empleados de ella

--- Api/src/Routes/companyRoute.js
+++ Api/src/Routes/companyRoute.js
@@ -2,12 +2,26 @@ const { Router } = require("express");
 const {
   createCompany,
   getCompanies,
+  getEmployees,
+  findCompany,
 } = require("../Controllers/CompanyController.js");
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   res.send(await getCompanies());
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  res.send(await findCompany(id));
+});
+
+router.get("/employees/:id", async (req, res) => {
+  const { id } = req.params;
+  const company = await findCompany(id);
+  const ids = company.employees;
+  res.send(await getEmployees(ids));
 });
 
 router.post("/login", async (req, res) => {
