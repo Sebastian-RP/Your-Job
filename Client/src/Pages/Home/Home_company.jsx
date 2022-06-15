0acdfcf46b988f6a9ede9c2439a6890fdf88f4bd
grexon-hack <jsarabialugo@gmail.com>
2022-06-14 19:58:34 -0500

    fix(/home_company)

--- Client/src/Pages/Home/Home_company.jsx
+++ Client/src/Pages/Home/Home_company.jsx
@@ -8,9 +8,8 @@ import {
   getAllPost,
   getAllPostsFromCompany,
 } from "../../Redux/Actions/Actions";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import style from "./home.module.css";
+import {Button, Card} from "react-bootstrap";
+import style from "./homeCompany.module.css";
 
 export default function HomeCompany() {
   const { user } = useAuth0();
@@ -31,13 +30,12 @@ export default function HomeCompany() {
   useEffect(() => {
     companies.forEach((comp) => {
       if (comp.email === user.email) {
-        console.log(comp);
         setCompany(comp);
-        console.log(company);
       }
     });
     // eslint-disable-next-line
   }, []);
+  
 
   useEffect(() => {
     if (company) {
@@ -45,32 +43,45 @@ export default function HomeCompany() {
     }
     // eslint-disable-next-line
   }, [company]);
-
+  console.log(user)
   return (
-    <>
+    <div className={style.containerCompany}>
       <Navbar />
-      <div className={style.columnPost}>
+      <div className={style.containerInfo}>
+        <div className={style.infoCompany}>
+          <h2>Company</h2>
+          <div className={style.imageCompany}>
+            <img src={company?.image} alt="image company" />
+          </div>
+          <div className={style.infoCompany}>
+            <p><strong>Name:</strong> {company?.name}</p>
+            <p><strong>Email:</strong> {company?.email}</p>
+            <p><strong>Address:</strong> {company?.address}</p>
+          </div>
+          <Button variant='success'>Create Post</Button>
+        </div>
+        <div className={style.infoPost}>
+
         {posts?.map((data, index) => {
-          console.log(data.id);
           return (
             <div className={style.cardPost} key={index}>
               <Card>
-                <Card.Header as="h5">Oferta Laboral</Card.Header>
+                <Card.Header as="h6">{data.titlePost}</Card.Header>
                 <Card.Body>
-                  <Card.Title>{data.TitlePost}</Card.Title>
+                  {/* <Card.Title>{data.TitlePost}</Card.Title> */}
                   <Card.Text style={{ textAlign: "start" }}>
-                    {data.descripcion}
-                    <br />
+                    <div className={style.info}>
+                      <span>
                     <strong>Experience:</strong> {data.experience}
+                      </span>
                     <br />
-                    <strong>Min-Salary:</strong> {data.min_salary}
-                    <br />
-                    <strong>Max-Salary:</strong> {data.max_salary}
-                    <br />
+                    <span>
                     <strong>Modality:</strong> {data.modality}
+                    </span>
                     <br />
-                    <strong>Technologies:</strong>
                     <>
+                    <strong>Technologies:</strong>
+                    <div className={style.ul}>
                       {data.technologiesId.map((data, i) => {
                         let tech = allTechnologies.find(
                           // eslint-disable-next-line
@@ -79,21 +90,26 @@ export default function HomeCompany() {
 
                         return <li key={i}>{tech ? tech.name : data}</li>;
                       })}
+                    </div>
                     </>
-                    <br />
+                    </div>
+                    
+                    {/* <br />
                     <strong>Postulates:</strong>
                     <>
                       {data.postulates?.map((data, i) => {
                         return <li key={i}> {data.name} </li>;
                       })}
-                    </>
+                    </> */}
                   </Card.Text>
+                  <p>Created: {data.createdAt.slice(0,10)}</p>
                 </Card.Body>
               </Card>
             </div>
           );
         })}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
