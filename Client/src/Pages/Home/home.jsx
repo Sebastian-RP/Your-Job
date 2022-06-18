2c5e80e8c22959aae351acd2fc6ae54b36108559
Luciano plaza <95538929+Luciano-plaza@users.noreply.github.com>
2022-06-17 23:20:12 -0300

    Update home.jsx

--- Client/src/Pages/Home/home.jsx
+++ Client/src/Pages/Home/home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
   const companies = [...selector.companies];
   const loggedUser = useSelector((state) => state.myUser);
   const loggedCompany = useSelector((state) => state.myCompany);
-  console.log(loggedCompany?)
+  console.log(loggedCompany)
   useEffect(() => {
     dispatch(getAllCompanies());
     dispatch(getAllPost());
