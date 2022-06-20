5aed25456d03b4fc533b5f09bb60afad298a64e9
Tomshaster <tomascartagena00@gmail.com>
2022-06-20 20:22:29 -0300

    Updated Model for CompanyPost
    modifiqué el modelo para que los enum acepten las nuevas variables de los mockeos

--- Api/src/models/CompanyPost.js
+++ Api/src/models/CompanyPost.js
@@ -1,49 +1,54 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('companyPost', {
-        titlePost: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        technologiesId:{
-            type: DataTypes.ARRAY(DataTypes.INTEGER)
-        },
-        experience:{
-            type: DataTypes.ENUM(["trainig", "junior", "semi-senior", "senior"]) //concordar los termino a usar
-        },
-        typeof_contract:{
-            type: DataTypes.ENUM(["por labor", "temporal", "termino fijo", "termino indefinido"])
-        },
-        companyId: { 
-            type: DataTypes.UUID //con el id podemos consultar el nombre de la empresa 
-        },
-        descripcion: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        min_salary: {
-            type: DataTypes.INTEGER,
-        },
-        max_salary:{ // rango salarial: min_salary - max_salary
-            type: DataTypes.INTEGER
-        },
-        modality:{
-            type: DataTypes.ENUM(["remoto", "presencial"])
-        },
-        status: {
-            type: DataTypes.ENUM(["active", "disabled"]),
-            defaultValue: "active"
-        }
-    })
+  sequelize.define("companyPost", {
+    titlePost: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    technologiesId: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+    },
+    experience: {
+      type: DataTypes.ENUM(["Training", "Junior", "Semi-Senior", "Senior"]), //concordar los termino a usar
+    },
+    typeof_contract: {
+      type: DataTypes.ENUM([
+        "Seasonal",
+        "Indeterminate",
+        "termino fijo",
+        "termino indefinido",
+      ]),
+    },
+    companyId: {
+      type: DataTypes.UUID, //con el id podemos consultar el nombre de la empresa
+    },
+    descripcion: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    min_salary: {
+      type: DataTypes.INTEGER,
+    },
+    max_salary: {
+      // rango salarial: min_salary - max_salary
+      type: DataTypes.INTEGER,
+    },
+    modality: {
+      type: DataTypes.ENUM(["Remote", "Presential"]),
+    },
+    status: {
+      type: DataTypes.ENUM(["active", "disabled"]),
+      defaultValue: "active",
+    },
+  });
 
-    sequelize.define("postulates", {
-        name: {
-            type: DataTypes.STRING
-        },
-        url: {
-            type: DataTypes.STRING
-        },
-
-    })
-}
+  sequelize.define("postulates", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+    },
+  });
+};
