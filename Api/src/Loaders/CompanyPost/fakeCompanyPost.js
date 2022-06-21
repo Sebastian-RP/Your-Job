d2a3726bdeb6b4afb7d3341659e4c45fcda829cb
Luciano plaza <95538929+Luciano-plaza@users.noreply.github.com>
2022-06-21 11:22:36 -0300

    Feature(api/src): Agregué el modelo base de admin y corregí variables

--- Api/src/Loaders/CompanyPost/fakeCompanyPost.js
+++ Api/src/Loaders/CompanyPost/fakeCompanyPost.js
@@ -1,15 +1,14 @@
 const { createPost } = require("../../Controllers/companyPostController.js");
-const { fakeCompanyPostData } = require("./arrayCompanyPosts");
 const { getIds } = require("./arrayCompanyPosts");
 
 const loaderCompanyPostData = async () => {
 
     try {
-        let count = 0
-        const lala = await getIds()
-        lala.forEach(user => {
+
+        const noPromise = await getIds()
+
+        noPromise.forEach(user => {
             createPost(user[0], user[1], user[2], user[3], user[4], user[5], user[6], user[7], user[8])
-            count++
         })
 
         console.log("data companyPost cargada");
