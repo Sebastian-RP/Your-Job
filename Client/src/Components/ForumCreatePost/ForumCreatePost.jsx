b6539285f26e294a8f84c4352070f7445db0c6b9
Jspena01 <josesamuelrd@gmail.com>
2022-06-26 18:52:16 -0300

    Feature-Forum(ForumCreatePost): Added functionality to the preview post

--- Client/src/Components/ForumCreatePost/ForumCreatePost.jsx
+++ Client/src/Components/ForumCreatePost/ForumCreatePost.jsx
@@ -4,13 +4,23 @@ import { useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import swal from "sweetalert";
-async function submitHandler(e, state, setState) {
+import Post from "../Post/Post.jsx";
+import { useNavigate } from "react-router-dom";
+async function submitHandler(e, state, setState, navigate) {
   e.preventDefault();
   try {
-    const response = await axios.post("/forum/post", { ...state });
-    if (response.status === 200) {
-      setState({ title: "", content: "", user: state.user });
-      swal({ title: "Create Post", text: "Your post has been posted", icon: "success" });
+    if (state.content.length <= 0) {
+      setState({ ...state, error: { ...state.error, content: "* Is required fill the content of the post *" } });
+    } else {
+      const response = await axios.post("/forum/post", { ...state });
+      if (response.status === 200) {
+        setState({ title: "", content: "", user: state.user });
+        swal({ title: "Create Post", text: "Your post has been posted", icon: "success" }).then((result) => {
+          if (result) {
+            navigate("/forum");
+          }
+        });
+      }
     }
   } catch (e) {
     swal({ title: "Create Post", text: "An error has ocurred", icon: "error" });
@@ -26,40 +36,59 @@ function changeHandler(e, state, setState) {
 
 export default function ForumCreatePost() {
   const { isAuthenticated, user } = useAuth0();
+  const navigate = useNavigate();
   const [state, setState] = useState({
     title: "",
     content: "",
     user: isAuthenticated ? user.name : "Guest",
     picture: isAuthenticated ? user.picture : "https://icon-library.com/images/profile-png-icon/profile-png-icon-2.jpg",
+    error: {},
   });
+  const [preview, setPreview] = useState(false);
+
   if (!user?.name && isAuthenticated) setState({ ...state, user: user.name });
   return (
     <section className={style.container}>
-      <h1>Create post</h1>
-      <form onSubmit={(e) => submitHandler(e, state, setState)}>
-        <div className={style.input__container}>
-          <label htmlFor="" className={style.label}>
-            Title
-          </label>
-          <input
-            className={style.input}
-            placeholder={"Insert a title for your post"}
-            type="text"
-            value={state.title}
-            onChange={(e) => changeHandler(e, state, setState)}
-            name="title"
-            id=""
-          />
-        </div>
-        <div className={style.input__container}>
-          <label htmlFor="" className={style.label}>
-            Body
-          </label>
-          <TextArea state={state} setState={setState} changeHandler={changeHandler} />
-        </div>
+      <h1>{!preview ? "Create post" : "Preview Post"}</h1>
+      <form onSubmit={(e) => submitHandler(e, state, setState, navigate)} className={!preview ? "" : style.form}>
+        {!preview ? (
+          <>
+            <div className={style.input__container}>
+              <label htmlFor="" className={style.label}>
+                Title
+              </label>
+              <input
+                className={style.input}
+                placeholder={"Insert a title for your post"}
+                type="text"
+                value={state.title}
+                onChange={(e) => changeHandler(e, state, setState)}
+                name="title"
+                id=""
+                required
+              />
+            </div>
+            <div className={style.input__container}>
+              <label htmlFor="" className={style.label}>
+                Body
+              </label>
+              <span className={style.error}>{state.error?.content ? state.error.content : ""}</span>
+              <TextArea state={state} setState={setState} changeHandler={changeHandler} />
+            </div>
+          </>
+        ) : (
+          <Post data={{ ...state, preview }} />
+        )}
         <div className={style.options__container}>
           <input type="submit" value="Create Post" className={style.button} />
-          <input type="button" value="Preview Post" className={style.button} />
+          <input
+            type="button"
+            value={preview ? "Edit Post" : "Preview Post"}
+            onClick={() => {
+              setPreview(!preview);
+            }}
+            className={style.button}
+          />
         </div>
       </form>
     </section>
--- Client/src/Components/Post/Post.jsx
+++ Client/src/Components/Post/Post.jsx
@@ -13,12 +13,11 @@ function getTimeAgo(hours) {
 }
 
 export default function Post({ data }) {
-  const { user, content, title, likes, dislikes, comments, createdAt, picture } = data;
+  const { user, content, title, likes = 0, dislikes = 0, comments = [], createdAt, picture, preview } = data;
   const date = new Date();
   const createdTime = createdAt;
   const currentTime = date.toISOString(date.getDate());
   const time = (new Date(currentTime).getTime() - new Date(createdTime).getTime()) / 1000 / 60 / 60;
-  console.log(data);
   return (
     <>
       <li className={style.post}>
@@ -26,11 +25,22 @@ export default function Post({ data }) {
           <img src={picture} className={style.profile__picture} alt="" />
           <p className={style.profile__user}>{user}</p>
           <p className={style.profile__time}>{getTimeAgo(time)}</p>
-          <button className={`${style.button} ${style.follow}`}>Follow</button>
+          <button
+            className={`${style.button} ${style.follow}`}
+            onClick={(e) => {
+              if (preview) {
+                e.preventDefault();
+              } else {
+                console.log("follow");
+              }
+            }}
+          >
+            Follow
+          </button>
         </div>
         <div className={style.container}>
-          <h4 className={style.title}>{title}</h4>
-          <p className={style.description}>{content}</p>
+          <h4 className={style.title}>{preview && title.length <= 0 ? "Title is empty" : title}</h4>
+          <p className={style.description}>{preview && content.length <= 0 ? "Content is empty" : content}</p>
           <button className={`${style.button} ${style.know}`}>Know more</button>
           <ul className={style.menu__interaction}>
             <li>
