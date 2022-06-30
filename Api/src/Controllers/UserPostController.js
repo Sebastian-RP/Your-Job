1d8cef3543877e714865afaa55a0432018c38258
Luciano plaza <95538929+Luciano-plaza@users.noreply.github.com>
2022-06-30 11:13:33 -0300

    Update UserPostController.js

--- Api/src/Controllers/UserPostController.js
+++ Api/src/Controllers/UserPostController.js
@@ -36,8 +36,8 @@ const getUserPosts = async () => {
     });
     Priority = Priority.map((A) => {
       A.sort((a, b) => {
-        if (a.user.premium === 1 && b.user.premium !== 1) return -1;
-        if (a.user.premium !== 1 && b.user.premium === 1) return 1;
+        if (a.user.premium === 1 || a.user.premium === 3 && b.user.premium !== 1 || b.user.premium !== 3) return -1;
+        if (a.user.premium !== 1 || a.user.premium !== 3 && b.user.premium === 1 || b.user.premium === 3) return 1;
         return 0;
       });
       return A;
